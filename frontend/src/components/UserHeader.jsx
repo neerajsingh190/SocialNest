815e31d87318283dd0a4bbd53b38c3a7b047e714
neerajsingh190 <112683517+neerajsingh190@users.noreply.github.com>
2024-10-02 19:15:56 +0530

    Update UserHeader.jsx

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -109,16 +109,6 @@ const UserHeader = ({ user }) => {
 				<Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb='3' cursor={"pointer"}>
 					<Text fontWeight={"bold"}>Posts</Text>
 				</Flex>
-				<Flex
-					flex={1}
-					borderBottom={"1px solid gray"}
-					justifyContent={"center"}
-					color={"gray.light"}
-					pb='3'
-					cursor={"pointer"}
-				>
-					<Text fontWeight={"bold"}> Replies</Text>
-				</Flex>
 			</Flex>
 		</VStack>
 	);
